5bce8ee5e89e6d15a7b2d7e39e3bea741f5239ad
cherifahmettidianediop <cherifahmettidianediop@gmail.com>
2024-12-20 08:35:42 +0000

    20/12/2024 08:35

---
+++ Calculatrice.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+//Declaration des variables 
+
+//Fonction addition 
+
+//Fonction multiplication 
+
+//Fonction soustraction 
+
+//Fonction Divition 
+
+main(){
+    
+}
